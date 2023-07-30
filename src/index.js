d1744c968ca598165be4ade33ed64269cfb2cdb0
James Douglas <jrdouglas27@gmail.com>
2023-07-30 22:10:01 +0100

    some deletion

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const myelement = (
+    <table>
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <td>Jimmy</td>
+      </tr>
+      <tr>
+        <td>Dougful</td>
+      </tr>
+    </table>
+  );
+  
+  const container = document.getElementById('root');
+  const root = ReactDOM.createRoot(container);
+  root.render(myelement);
